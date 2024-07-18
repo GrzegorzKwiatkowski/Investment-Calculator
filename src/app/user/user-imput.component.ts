c09f92a93fbc30f6b68f66bee4d2263139c84eb5
Grzegorz Kwiatkowski <beo.kwiatkowski@gmail.com>
2024-07-18 20:21:13 +0200

    Adding onCalculateInvestmentResult method

--- src/app/user/user-imput.component.ts
+++ src/app/user/user-imput.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-imput',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-imput.component.html',
   styleUrl: './user-imput.component.css'
 })
 
 
 export class UserImputComponent {
+  @Output() calculate = new EventEmitter<{
+    initialInvestment: number;
+    duration: number;
+    expectedReturn: number;
+    annualInvestment: number;
+  }>();
 
+  initialInvestmentValue = '0';
+  annualInvestmentValue = '0';
+  expectedReturnValue = '5';
+  durationValue = '10';
+onSubmit() {
+this.calculate.emit({
+  initialInvestment: +this.annualInvestmentValue,
+  duration: +this.durationValue,
+  expectedReturn: +this.expectedReturnValue,
+  annualInvestment: +this.annualInvestmentValue
+})
+}
 }
