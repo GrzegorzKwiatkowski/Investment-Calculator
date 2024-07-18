430bb9c8561b3e8df4f3ef75becdf5858b6cfb32
Grzegorz Kwiatkowski <beo.kwiatkowski@gmail.com>
2024-07-18 18:34:00 +0200

    Added header and user-imput components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { UserImputComponent } from './user/user-imput.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [HeaderComponent, UserImputComponent]
 })
 export class AppComponent {}
---
+++ src/app/user/user-imput.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-imput',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-imput.component.html',
+  styleUrl: './user-imput.component.css'
+})
+export class UserImputComponent {
+
+}
